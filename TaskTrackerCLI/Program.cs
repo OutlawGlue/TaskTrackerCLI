9db61380ee3b7d1df72bedf42c414b968cb9dabc
Matthew Irvine <irvinematthew28@gmail.com>
2025-08-30 15:38:38 +0100

    Created a basic menu.

--- TaskTrackerCLI/Program.cs
+++ TaskTrackerCLI/Program.cs
@@ -8,8 +8,41 @@ namespace TaskTrackerCLI
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Menu();
+        }
+
+        private static void Menu()
+        {
+            int input = 0;
+
+            do
+            {
+                Console.WriteLine("Main Menu:" +
+                "\n1: Add task" +
+                "\n2: View tasks" +
+                "\n3: Delete a task" +
+                "\n4: Exit");
+                try
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Invalid input. {error.Message}" +
+                        $"\nPlease try again.");
+                }
+            } while (input < 1 || input > 4);
+
+            switch (input)
+            {
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: Environment.Exit(0); break;
+            }
         }
     }
 }
